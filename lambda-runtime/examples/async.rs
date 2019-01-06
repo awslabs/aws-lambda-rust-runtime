2f6e599e269ec99be0d98df0a4e88a5d8f18eb11
Daniel Wagner-Hall <dawagner@gmail.com>
2019-01-06 10:54:21 +0000

    Add contrived async Handler example

---
+++ lambda-runtime/examples/async.rs
@@ -0,0 +1,30 @@
+use std::error::Error;
+
+use lambda_runtime::{error::HandlerError, lambda, Context};
+use serde_derive::{Deserialize, Serialize};
+use simple_logger;
+use tokio::prelude::future::{Future, ok};
+
+#[derive(Deserialize)]
+struct CustomEvent {
+    #[serde(rename = "firstName")]
+    first_name: String,
+}
+
+#[derive(Serialize)]
+struct CustomOutput {
+    message: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    simple_logger::init_with_level(log::Level::Debug).unwrap();
+    lambda!(my_handler);
+
+    Ok(())
+}
+
+fn my_handler(e: CustomEvent, _c: Context) -> impl Future<Item=CustomOutput, Error=HandlerError> {
+    ok(format!("Hello, {}!", e.first_name))
+        .map(|message| format!("{} (modified in a Future)", message))
+        .map(|message| CustomOutput { message })
+}

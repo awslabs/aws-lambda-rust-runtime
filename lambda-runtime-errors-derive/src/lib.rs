315c979a359d65d8edae9d974785643b139f7688
sapessi <stefano.buliani@gmail.com>
2019-01-24 09:29:58 -0800

    Removed commented code as per CR

--- lambda-runtime-errors-derive/src/lib.rs
+++ lambda-runtime-errors-derive/src/lib.rs
@@ -5,23 +5,6 @@ use synstructure::decl_derive;
 decl_derive!([LambdaErrorExt, attributes()] => lambda_error_derive);
 
 fn lambda_error_derive(s: synstructure::Structure) -> TokenStream {
-    /*
-    if let Data::Struct(d) = &s.ast().data {
-        if let Fields::Named(fs) = &d.fields {
-            for f in fs.named.iter() {
-                let field_name = f.ident.clone();
-                println!("!!Field name: {}", field_name.expect("Field must be named"));
-            }
-        }
-    }*/
-
-    for v in s.variants() {
-        println!("Variant: {}", v.ast().ident);
-        /*if let Some(id) = v.prefix {
-            println!("Variant: {}", id);
-        }*/    
-    }
-
     let name = format!("{}", s.ast().ident);
 
     let err_impl = s.gen_impl(quote! {

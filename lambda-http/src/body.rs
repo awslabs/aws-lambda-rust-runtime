7d037379b9461d2e50bf37c3a5709d620ee3f932
softprops <d.tangren@gmail.com>
2020-04-24 23:06:30 -0400

    cargo clippy tipoff on ellided lifetime

--- lambda-http/src/body.rs
+++ lambda-http/src/body.rs
@@ -75,7 +75,7 @@ impl Body {
     ///
     /// Panics when aws communicates to handler that request is base64 encoded but
     /// it can not be base64 decoded
-    pub(crate) fn from_maybe_encoded<'a>(is_base64_encoded: bool, body: Cow<'a, str>) -> Body {
+    pub(crate) fn from_maybe_encoded(is_base64_encoded: bool, body: Cow<'_, str>) -> Body {
         if is_base64_encoded {
             Body::from(::base64::decode(body.as_ref()).expect("failed to decode aws base64 encoded body"))
         } else {

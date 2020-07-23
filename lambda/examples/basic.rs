5f63aa952a4e22b386f657620e06b7c7d410f5b3
mx <max@onebro.me>
2020-07-23 12:53:39 +1200

    Corrected handler_fn interface misconception in basic.rs
    Co-authored-by: Veetaha <veetaha2@gmail.com>

--- lambda/examples/basic.rs
+++ lambda/examples/basic.rs
@@ -18,14 +18,9 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-pub(crate) async fn my_handler(event: Value, ctx: Context) -> Result<Value, Error> {
+pub(crate) async fn my_handler(event: Request, ctx: Context) -> Result<Response, Error> {
     // extract some useful info from the request
-    let command = match serde_json::from_value::<Request>(event) {
-        Err(e) => {
-            return Err(Box::new(e));
-        }
-        Ok(v) => v.command,
-    };
+    let command = event.command;
 
     // prepare the response
     let resp = Response {
@@ -33,13 +28,13 @@ pub(crate) async fn my_handler(event: Value, ctx: Context) -> Result<Value, Erro
         msg: format!("Command {} executed.", command),
     };
 
-    // return `Response` as JSON
-    Ok(json!(resp))
+    // return `Response` (it will be serialized to JSON automatically by the runtime)
+    Ok(resp)
 }
 
-/// This is a made up example of what a response structure may look like.
+/// This is a made-up example of what a response structure may look like.
 /// There is no restriction on what it can be. The runtime requires responses
-/// to be serialized into `serde_json::Value`. The runtime pays no attention
+/// to be serialized into json. The runtime pays no attention
 /// to the contents of the response payload.
 #[derive(Serialize)]
 struct Response {
@@ -47,10 +42,9 @@ struct Response {
     msg: String,
 }
 
-/// This is also a made up example. Requests come into the runtime as unicode
-/// strings and are converted into `serde_json::Value`, which can map to your
-/// own structures. The runtime pays no attention to the
-/// contents of the request payload.
+/// This is also a made-up example. Requests come into the runtime as unicode
+/// strings in json format, which can map to any structure that implements `serde::Deserialize`
+/// The runtime pays no attention to the contents of the request payload.
 #[derive(Deserialize)]
 struct Request {
     command: String,

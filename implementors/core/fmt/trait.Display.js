6b85e9158a4eaa8a47eac536cfd6a791332671b1
davidbarsky <davidbarsky@users.noreply.github.com>
2020-10-27 20:47:23 +0000

    deploy: 13aa8f01813e6949ca91aedafb286591fdf32217

--- implementors/core/fmt/trait.Display.js
+++ implementors/core/fmt/trait.Display.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["lambda_http"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"lambda_http/ext/enum.PayloadError.html\" title=\"enum lambda_http::ext::PayloadError\">PayloadError</a>","synthetic":false,"types":["lambda_http::ext::PayloadError"]}];
+implementors["lambda_http"] = [{"text":"impl Display for PayloadError","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/core/ops/deref/trait.Deref.js
+++ implementors/core/ops/deref/trait.Deref.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["lambda_http"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"enum\" href=\"lambda_http/enum.Body.html\" title=\"enum lambda_http::Body\">Body</a>","synthetic":false,"types":["lambda_http::body::Body"]}];
+implementors["lambda_http"] = [{"text":"impl Deref for Body","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

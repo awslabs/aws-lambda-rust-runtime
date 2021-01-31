5ae037ef9ffabdac5a2cfcd22cd2c090e104e914
Colton Weaver <coltonbweaver@gmail.com>
2021-01-30 20:42:05 -0800

    Remove lambda attributes (#282)

--- lambda/examples/hello.rs
+++ lambda/examples/hello.rs
@@ -1,10 +1,17 @@
-use lambda::{lambda, Context};
-use serde_json::Value;
+use lambda::{handler_fn, Context};
+use serde_json::{json, Value};
 
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
-#[lambda]
 #[tokio::main]
-async fn main(event: Value, _: Context) -> Result<Value, Error> {
-    Ok(event)
+async fn main() -> Result<(), Error> {
+    let func = handler_fn(func);
+    lambda::run(func).await?;
+    Ok(())
+}
+
+async fn func(event: Value, _: Context) -> Result<Value, Error> {
+    let first_name = event["firstName"].as_str().unwrap_or("world");
+
+    Ok(json!({ "message": format!("Hello, {}!", first_name) }))
 }

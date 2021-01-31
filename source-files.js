0772c2ec0ac2878f11e56aae3b8c6a8c308a0dbf
coltonweaver <coltonweaver@users.noreply.github.com>
2021-01-31 04:46:05 +0000

    deploy: 5ae037ef9ffabdac5a2cfcd22cd2c090e104e914

--- source-files.js
+++ source-files.js
@@ -1,5 +1,4 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["lambda"] = {"name":"","files":["client.rs","lib.rs","requests.rs","simulated.rs","types.rs"]};
-sourcesIndex["lambda_attributes"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["lambda_http"] = {"name":"","files":["body.rs","ext.rs","lib.rs","request.rs","response.rs","strmap.rs"]};
 createSourceSidebar();

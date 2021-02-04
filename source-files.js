3cc062ec52b68e9444d60474850ebca240fc7626
bahildebrand <bahildebrand@users.noreply.github.com>
2021-02-04 02:50:44 +0000

    deploy: e0803e18676d05ca51e714eb84a23f1bea53f887

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["lambda"] = {"name":"","files":["client.rs","lib.rs","requests.rs","simulated.rs","types.rs"]};
+sourcesIndex["lambda"] = {"name":"","files":["client.rs","lib.rs","requests.rs","types.rs"]};
 sourcesIndex["lambda_http"] = {"name":"","files":["body.rs","ext.rs","lib.rs","request.rs","response.rs","strmap.rs"]};
 createSourceSidebar();

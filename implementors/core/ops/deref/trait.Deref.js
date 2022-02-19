85bc1692d9724fdb5f41e011feb0a580b16935db
nmoutschen <nmoutschen@users.noreply.github.com>
2022-02-19 18:31:08 +0000

    deploy: 3d5565df8590b0024d04aa5dbecd081e0d335972

--- implementors/core/ops/deref/trait.Deref.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["lambda_http"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.58.1/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"enum\" href=\"lambda_http/enum.Body.html\" title=\"enum lambda_http::Body\">Body</a>","synthetic":false,"types":["lambda_http::body::Body"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- logs_trait/sidebar-items.js
+++ logs_trait/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["main",""]],"struct":[["MyLogsProcessor","Custom log processor that increments a counter for each log record."]]});
+initSidebarItems({"fn":[["main",""]],"struct":[["MyLogsProcessor","Custom log processor that increments a counter for each log record."]],"type":[["MyLogsFuture",""]]});

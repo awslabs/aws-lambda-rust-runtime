e1797cc09a0d1339d40ab9b49108599595049897
Nicolas Moutschen <nicolas.moutschen@gmail.com>
2022-02-19 18:22:31 +0100

    feat(lambda-http): add example with tower_http crate (#425)
    * feat(lambda-runtime): add all features for tower re-export
    
    * feat(lambda-http): add CORS layer example
    
    * revert(lambda-http): revert changes to LambdaRequest and Body types

---
+++ lambda-http/examples/hello-cors.rs
@@ -0,0 +1,29 @@
+use http::Method;
+use lambda_http::{service_fn, tower::ServiceBuilder, Body, Error, IntoResponse, Request, RequestExt, Response};
+use tower_http::cors::{Any, CorsLayer};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    // Define a layer to inject CORS headers
+    let cors_layer = CorsLayer::new()
+        .allow_methods(vec![Method::GET, Method::POST])
+        .allow_origin(Any);
+
+    let handler = ServiceBuilder::new()
+        // Add the CORS layer to the service
+        .layer(cors_layer)
+        .service(service_fn(func));
+
+    lambda_http::run(handler).await?;
+    Ok(())
+}
+
+async fn func(event: Request) -> Result<Response<Body>, Error> {
+    Ok(match event.query_string_parameters().get("first_name") {
+        Some(first_name) => format!("Hello, {}!", first_name).into_response(),
+        _ => Response::builder()
+            .status(400)
+            .body("Empty first name".into())
+            .expect("failed to render response"),
+    })
+}

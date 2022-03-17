78eeb5c98ad19fa6708eace9ce2306a4568a0c08
David Calavera <david.calavera@gmail.com>
2022-03-16 21:44:52 -0700

    Enable http niciness only when binaries are compiled in debug mode.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- lambda-runtime/src/lib.rs
+++ lambda-runtime/src/lib.rs
@@ -102,6 +102,7 @@ where
             let event = next_event_response?;
             let (parts, body) = event.into_parts();
 
+            #[cfg(debug_assertions)]
             if parts.status == http::StatusCode::NO_CONTENT {
                 // Ignore the event if the status code is 204.
                 // This is a way to keep the runtime alive when
@@ -112,6 +113,7 @@ where
             let body = hyper::body::to_bytes(body).await?;
             trace!("response body - {}", std::str::from_utf8(&body)?);
 
+            #[cfg(debug_assertions)]
             if parts.status.is_server_error() {
                 error!("Lambda Runtime server returned an unexpected error");
                 return Err(parts.status.to_string().into());

1f8c4daffa9e1c4edbc1076002b2a100624f5537
sgasse <47532708+sgasse@users.noreply.github.com>
2022-09-05 19:01:15 +0200

    Fix trivial typo in docs of RequestExt (#525)
    Co-authored-by: Simon Gasse <sgasse@users.noreply.github.com>

--- lambda-http/src/ext.rs
+++ lambda-http/src/ext.rs
@@ -162,7 +162,7 @@ pub trait RequestExt {
     where
         V: Into<QueryMap>;
 
-    /// Return request context data assocaited with the ALB or API gateway request
+    /// Return request context data associated with the ALB or API gateway request
     fn request_context(&self) -> RequestContext;
 
     /// Configures instance with request context

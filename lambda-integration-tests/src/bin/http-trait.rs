97ed59d2a574fda1856f6a6436414a259f3e2cd4
Daniel Cormier <dcormier@users.noreply.github.com>
2022-10-06 10:13:55 -0400

    Check that `lambda_http`s inner service is ready before calling it
    This allows for service [backpressure](https://docs.rs/tower/0.4.13/tower/trait.Service.html#backpressure).
    
    Additionally:
    
    > Services are permitted to panic if `call` is invoked without obtaining `Poll::Ready(Ok(()))` from `poll_ready`.
    
    [Source](https://docs.rs/tower/0.4.13/tower/trait.Service.html#be-careful-when-cloning-inner-services)

--- lambda-integration-tests/src/bin/http-trait.rs
+++ lambda-integration-tests/src/bin/http-trait.rs
@@ -1,13 +1,36 @@
-use lambda_http::{Body, Error, Request, RequestExt, Response, Service};
 use std::{
     future::{ready, Future},
     pin::Pin,
+    sync::atomic::{AtomicBool, Ordering},
 };
+
+use lambda_http::{Body, Error, Request, RequestExt, Response, Service};
 use tracing::info;
 
-#[derive(Default)]
 struct MyHandler {
     invoke_count: usize,
+    ready: AtomicBool,
+}
+
+impl Default for MyHandler {
+    fn default() -> Self {
+        Self {
+            invoke_count: usize::default(),
+            // New instances are not ready to be called until polled.
+            ready: false.into(),
+        }
+    }
+}
+
+impl Clone for MyHandler {
+    fn clone(&self) -> Self {
+        Self {
+            invoke_count: self.invoke_count,
+            // Cloned instances may not be immediately ready to be called.
+            // https://docs.rs/tower/0.4.13/tower/trait.Service.html#be-careful-when-cloning-inner-services
+            ready: false.into(),
+        }
+    }
 }
 
 impl Service<Request> for MyHandler {
@@ -16,6 +39,12 @@ impl Service<Request> for MyHandler {
     type Response = Response<Body>;
 
     fn poll_ready(&mut self, _cx: &mut core::task::Context<'_>) -> core::task::Poll<Result<(), Self::Error>> {
+        if self.ready.swap(true, Ordering::SeqCst) {
+            info!("[http-trait] Service was already ready");
+        } else {
+            info!("[http-trait] Service is now ready");
+        };
+
         core::task::Poll::Ready(Ok(()))
     }
 
@@ -23,6 +52,18 @@ impl Service<Request> for MyHandler {
         self.invoke_count += 1;
         info!("[http-trait] Received event {}: {:?}", self.invoke_count, request);
         info!("[http-trait] Lambda context: {:?}", request.lambda_context());
+
+        // After being called once, the service is no longer ready until polled again.
+        if self.ready.swap(false, Ordering::SeqCst) {
+            info!("[http-trait] The service is ready");
+        } else {
+            // https://docs.rs/tower/latest/tower/trait.Service.html#backpressure
+            // https://docs.rs/tower/latest/tower/trait.Service.html#be-careful-when-cloning-inner-services
+            // > Services are permitted to panic if `call` is invoked without obtaining
+            // > `Poll::Ready(Ok(()))` from `poll_ready`.
+            panic!("[http-trait] The service is not ready; `.poll_ready()` must be called first");
+        }
+
         Box::pin(ready(Ok(Response::builder()
             .status(200)
             .body(Body::from("Hello, World!"))

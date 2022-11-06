19463d4bc3bfa8a04b43965d36ee324498a2fdba
Justin Knoetzke <jfk@amazon.com>
2022-11-06 21:05:23 +0200

    Code formatting. Implemented clone on struct.

--- examples/http-dynamodb/src/main.rs
+++ examples/http-dynamodb/src/main.rs
@@ -1,6 +1,6 @@
-use lambda_http::{run, service_fn, Error, IntoResponse, Request};
 use aws_sdk_dynamodb::model::AttributeValue;
 use aws_sdk_dynamodb::{Client, Error as OtherError};
+use lambda_http::{run, service_fn, Error, IntoResponse, Request};
 use tracing::info;
 
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -12,33 +12,41 @@ pub struct Item {
     pub last: String,
 }
 
+impl Clone for Item {
+    fn clone(&self) -> Item {
+        return Item {
+            p_type: self.p_type.clone(),
+            age: self.age.clone(),
+            username: self.username.clone(),
+            first: self.first.clone(),
+            last: self.last.clone(),
+        };
+    }
+}
 /// This is the main body for the function.
 /// Write your code inside it.
 /// You can see more examples in Runtime's repository:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 async fn function_handler(event: Request) -> Result<impl IntoResponse, Error> {
-  
     // Extract some useful information from the request
     let body = event.body();
     let s = std::str::from_utf8(&body).expect("invalid utf-8 sequence");
     //Log into Cloudwatch
     info!(payload = %s, "JSON Payload received");
-  
+
     //Serialize the data into the struct.
     let item: Item = serde_json::from_str(s).map_err(Box::new)?;
-    //Create a copy to send back via the Response.
-    let copy_item: Item = serde_json::from_str(s).map_err(Box::new)?;  
 
     //Setup the client to write to DynamoDB
-    let config = aws_config::load_from_env().await; 
+    let config = aws_config::load_from_env().await;
     let client = Client::new(&config);
 
     //Insert into the table.
-    add_item(&client, item, "lambda_dyno_example").await?;   
- 
+    add_item(&client, item.clone(), "lambda_dyno_example").await?;
+
     //Deserialize into json to return in the Response
-    let j = serde_json::to_string(&copy_item)?;
- 
+    let j = serde_json::to_string(&item)?;
+
     Ok(j)
 }
 
@@ -55,8 +63,8 @@ async fn main() -> Result<(), Error> {
 
 // Add an item to a table.
 // snippet-start:[dynamodb.rust.add-item]
-pub async fn add_item(client: &Client, item: Item, table: &str) -> Result<(), OtherError> 
-{   let user_av = AttributeValue::S(item.username);
+pub async fn add_item(client: &Client, item: Item, table: &str) -> Result<(), OtherError> {
+    let user_av = AttributeValue::S(item.username);
     let type_av = AttributeValue::S(item.p_type);
     let age_av = AttributeValue::S(item.age);
     let first_av = AttributeValue::S(item.first);
@@ -72,7 +80,7 @@ pub async fn add_item(client: &Client, item: Item, table: &str) -> Result<(), Ot
         .item("last_name", last_av);
 
     info!("adding item to DynamoDB");
-   
+
     let _resp = request.send().await?;
 
     Ok(())

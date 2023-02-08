1dda1cceca7b9151ef8d93b7565c6104b4d75e67
Christopher Greenwood <greenwd@amazon.com>
2023-02-08 09:45:16 -0800

    add example for Lambda + HTTP + Axum
    this example bridges Lambda's HTTP support, which is based on the `tower::Service` trait,
    and the axum web framework, which builds applications that are also based on the
    `tower::Service` trait.

---
+++ examples/http-axum/src/main.rs
@@ -0,0 +1,52 @@
+//! This is an example function that leverages the Lambda Rust runtime's HTTP support
+//! and the [axum](https://docs.rs/axum/latest/axum/index.html) web framework.  The
+//! runtime HTTP support is backed by the [tower::Service](https://docs.rs/tower-service/0.3.2/tower_service/trait.Service.html)
+//! trait.  Axum applications are also backed by the `tower::Service` trait.  That means
+//! that it is fairly easy to build an Axum application and pass the resulting `Service`
+//! implementation to the Lambda runtime to run as a Lambda function.  By using Axum instead
+//! of a basic `tower::Service` you get web framework niceties like routing, request component
+//! extraction, validation, etc.
+
+use lambda_http::{
+    run,
+    Error,
+};
+use axum::{
+    extract::Path,
+    response::Json,
+    Router,
+    routing::{get, post},
+};
+use serde_json::{Value, json};
+
+async fn root() -> Json<Value> {
+    Json(json!({ "msg": "I am GET /" }))
+}
+
+async fn get_foo() -> Json<Value> {
+    Json(json!({ "msg": "I am GET /foo" }))
+}
+
+async fn post_foo() -> Json<Value> {
+    Json(json!({ "msg": "I am POST /foo" }))
+}
+
+async fn post_foo_name(Path(name): Path<String>) -> Json<Value> {
+    Json(json!({ "msg": format!("I am POST /foo/:name, name={name}") }))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        // disabling time is handy because CloudWatch will add the ingestion time.
+        .without_time()
+        .init();
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/foo", get(get_foo).post(post_foo))
+        .route("/foo/:name", post(post_foo_name));
+
+    run(app).await
+}

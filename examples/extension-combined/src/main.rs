847b060c3947eec14c264fcaab5da24dccb4ceea
greenwoodcm <greenwd@amazon.com>
2023-03-01 07:03:58 -0800

    make tracing init consistent across examples (#609)

--- examples/extension-combined/src/main.rs
+++ examples/extension-combined/src/main.rs
@@ -29,10 +29,14 @@ async fn my_log_processor(logs: Vec<LambdaLog>) -> Result<(), Error> {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // The runtime logging can be enabled here by initializing `tracing` with `tracing-subscriber`
-    // While `tracing` is used internally, `log` can be used as well if preferred.
+    // required to enable CloudWatch error logging by the runtime
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
+        // disable printing the name of the module in every log line.
+        .with_target(false)
+        // this needs to be set to false, otherwise ANSI color codes will
+        // show up in a confusing manner in CloudWatch logs.
+        .with_ansi(false)
         // disabling time is handy because CloudWatch will add the ingestion time.
         .without_time()
         .init();

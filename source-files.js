62a31ab5fbaaeaeffba269c4fa7cf8f053974cfa
bnusunny <bnusunny@users.noreply.github.com>
2023-04-09 05:12:50 +0000

    deploy: 3ee4389df868fa5365eb800ac3eb3969a7d1320f

--- source-files.js
+++ source-files.js
@@ -4,8 +4,8 @@ var sourcesIndex = JSON.parse('{\
 "http_fn":["",[],["http-fn.rs"]],\
 "http_trait":["",[],["http-trait.rs"]],\
 "lambda_extension":["",[],["error.rs","events.rs","extension.rs","lib.rs","logs.rs","requests.rs","telemetry.rs"]],\
-"lambda_http":["",[],["ext.rs","lib.rs","request.rs","response.rs"]],\
-"lambda_runtime":["",[],["lib.rs","requests.rs","types.rs"]],\
+"lambda_http":["",[],["ext.rs","lib.rs","request.rs","response.rs","streaming.rs"]],\
+"lambda_runtime":["",[],["lib.rs","requests.rs","streaming.rs","types.rs"]],\
 "lambda_runtime_api_client":["",[],["lib.rs"]],\
 "logs_trait":["",[],["logs-trait.rs"]],\
 "runtime_fn":["",[],["runtime-fn.rs"]],\

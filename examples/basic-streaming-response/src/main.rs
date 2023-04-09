356feda0e2882741bc950b4b41bd40c5e5335c68
Harold Sun <sunhua@amazon.com>
2023-04-09 01:42:06 +0000

    Add basic-streaming-response example

---
+++ examples/basic-streaming-response/src/main.rs
@@ -0,0 +1,44 @@
+use hyper::{body::Body, Response};
+use lambda_runtime::{service_fn, Error, LambdaEvent};
+use serde_json::Value;
+use std::{thread, time::Duration};
+
+
+async fn func(_event: LambdaEvent<Value>) -> Result<Response<Body>, Error> {
+    let messages = vec!["Hello", "world", "from", "Lambda!"];
+
+    let (mut tx, rx) = Body::channel();
+
+    tokio::spawn(async move {
+        for message in messages.iter() {
+            tx.send_data((message.to_string() + "\n").into()).await.unwrap();
+            thread::sleep(Duration::from_millis(500));
+        }
+    });
+
+    let resp = Response::builder()
+        .header("content-type", "text/html")
+        .header("CustomHeader", "outerspace")
+        .body(rx)?;
+
+    Ok(resp)
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    // required to enable CloudWatch error logging by the runtime
+    tracing_subscriber::fmt()
+    .with_max_level(tracing::Level::INFO)
+    // disable printing the name of the module in every log line.
+    .with_target(false)
+    // this needs to be set to false, otherwise ANSI color codes will
+    // show up in a confusing manner in CloudWatch logs.
+    .with_ansi(false)
+    // disabling time is handy because CloudWatch will add the ingestion time.
+    .without_time()
+    .init();
+
+    lambda_runtime::run_with_streaming_response(service_fn(func)).await?;
+    Ok(())
+}

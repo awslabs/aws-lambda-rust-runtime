49fc028980cdf4f0451a3f2edbb40906d9dedd6a
calavera <calavera@users.noreply.github.com>
2023-04-14 15:26:01 +0000

    deploy: c5d6f1ecf30cadf19aee2f2d5ef3f911d1b8fd00

--- source-files.js
+++ source-files.js
@@ -5,7 +5,7 @@ var sourcesIndex = JSON.parse('{\
 "http_trait":["",[],["http-trait.rs"]],\
 "lambda_extension":["",[],["error.rs","events.rs","extension.rs","lib.rs","logs.rs","requests.rs","telemetry.rs"]],\
 "lambda_http":["",[["ext",[],["extensions.rs","mod.rs","request.rs"]]],["lib.rs","request.rs","response.rs","streaming.rs"]],\
-"lambda_runtime":["",[],["lib.rs","requests.rs","streaming.rs","types.rs"]],\
+"lambda_runtime":["",[],["deserializer.rs","lib.rs","requests.rs","streaming.rs","types.rs"]],\
 "lambda_runtime_api_client":["",[],["lib.rs"]],\
 "logs_trait":["",[],["logs-trait.rs"]],\
 "runtime_fn":["",[],["runtime-fn.rs"]],\

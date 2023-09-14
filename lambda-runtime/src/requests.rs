a89f507d557ec0213db7038a44b9bd234fe7f397
Harold Sun <sunhua@amazon.com>
2023-09-15 00:27:44 +0800

    Refactor Lambda response streaming.
    Remove the separate streaming.rs from lambda-runtime crate. Merge into the `run` method.
    
    Added FunctionResponse enum to capture both buffered response and streaming response.
    
    Added IntoFunctionResponse trait to convert `Serialize` response into FunctionResponse::BufferedResponse, and convert `Stream` response into FunctionResponse::StreamingResponse. Existing handler functions should continue to work.
    
    Improved error handling in response streaming. Return trailers to report errors instead of panic.

--- lambda-runtime/src/requests.rs
+++ lambda-runtime/src/requests.rs
@@ -1,9 +1,15 @@
-use crate::{types::Diagnostic, Error};
+use crate::types::ToStreamErrorTrailer;
+use crate::{types::Diagnostic, Error, FunctionResponse, IntoFunctionResponse};
+use bytes::Bytes;
+use http::header::CONTENT_TYPE;
 use http::{Method, Request, Response, Uri};
 use hyper::Body;
 use lambda_runtime_api_client::build_request;
 use serde::Serialize;
+use std::fmt::Debug;
+use std::marker::PhantomData;
 use std::str::FromStr;
+use tokio_stream::{Stream, StreamExt};
 
 pub(crate) trait IntoRequest {
     fn into_req(self) -> Result<Request<Body>, Error>;
@@ -65,23 +71,85 @@ fn test_next_event_request() {
 }
 
 // /runtime/invocation/{AwsRequestId}/response
-pub(crate) struct EventCompletionRequest<'a, T> {
+pub(crate) struct EventCompletionRequest<'a, R, B, S, D, E>
+where
+    R: IntoFunctionResponse<B, S>,
+    B: Serialize,
+    S: Stream<Item = Result<D, E>> + Unpin + Send + 'static,
+    D: Into<Bytes> + Send,
+    E: Into<Error> + Send + Debug,
+{
     pub(crate) request_id: &'a str,
-    pub(crate) body: T,
+    pub(crate) body: R,
+    pub(crate) _unused_b: PhantomData<B>,
+    pub(crate) _unused_s: PhantomData<S>,
 }
 
-impl<'a, T> IntoRequest for EventCompletionRequest<'a, T>
+impl<'a, R, B, S, D, E> IntoRequest for EventCompletionRequest<'a, R, B, S, D, E>
 where
-    T: for<'serialize> Serialize,
+    R: IntoFunctionResponse<B, S>,
+    B: Serialize,
+    S: Stream<Item = Result<D, E>> + Unpin + Send + 'static,
+    D: Into<Bytes> + Send,
+    E: Into<Error> + Send + Debug,
 {
     fn into_req(self) -> Result<Request<Body>, Error> {
-        let uri = format!("/2018-06-01/runtime/invocation/{}/response", self.request_id);
-        let uri = Uri::from_str(&uri)?;
-        let body = serde_json::to_vec(&self.body)?;
-        let body = Body::from(body);
+        match self.body.into_response() {
+            FunctionResponse::BufferedResponse(body) => {
+                let uri = format!("/2018-06-01/runtime/invocation/{}/response", self.request_id);
+                let uri = Uri::from_str(&uri)?;
 
-        let req = build_request().method(Method::POST).uri(uri).body(body)?;
-        Ok(req)
+                let body = serde_json::to_vec(&body)?;
+                let body = Body::from(body);
+
+                let req = build_request().method(Method::POST).uri(uri).body(body)?;
+                Ok(req)
+            }
+            FunctionResponse::StreamingResponse(mut response) => {
+                let uri = format!("/2018-06-01/runtime/invocation/{}/response", self.request_id);
+                let uri = Uri::from_str(&uri)?;
+
+                let mut builder = build_request().method(Method::POST).uri(uri);
+                let req_headers = builder.headers_mut().unwrap();
+
+                req_headers.insert("Transfer-Encoding", "chunked".parse()?);
+                req_headers.insert("Lambda-Runtime-Function-Response-Mode", "streaming".parse()?);
+                req_headers.append("Trailer", "Lambda-Runtime-Function-Error-Type".parse()?);
+                req_headers.append("Trailer", "Lambda-Runtime-Function-Error-Body".parse()?);
+                req_headers.insert(
+                    "Content-Type",
+                    "application/vnd.awslambda.http-integration-response".parse()?,
+                );
+
+                // default Content-Type
+                let preloud_headers = &mut response.metadata_prelude.headers;
+                preloud_headers
+                    .entry(CONTENT_TYPE)
+                    .or_insert("application/octet-stream".parse()?);
+
+                let metadata_prelude = serde_json::to_string(&response.metadata_prelude)?;
+
+                tracing::trace!(?metadata_prelude);
+
+                let (mut tx, rx) = Body::channel();
+
+                tokio::spawn(async move {
+                    tx.send_data(metadata_prelude.into()).await.unwrap();
+                    tx.send_data("\u{0}".repeat(8).into()).await.unwrap();
+
+                    while let Some(chunk) = response.stream.next().await {
+                        let chunk = match chunk {
+                            Ok(chunk) => chunk.into(),
+                            Err(err) => err.into().to_tailer().into(),
+                        };
+                        tx.send_data(chunk).await.unwrap();
+                    }
+                });
+
+                let req = builder.body(rx)?;
+                Ok(req)
+            }
+        }
     }
 }
 
@@ -90,6 +158,8 @@ fn test_event_completion_request() {
     let req = EventCompletionRequest {
         request_id: "id",
         body: "hello, world!",
+        _unused_b: PhantomData::<&str>,
+        _unused_s: PhantomData::<Body>,
     };
     let req = req.into_req().unwrap();
     let expected = Uri::from_static("/2018-06-01/runtime/invocation/id/response");

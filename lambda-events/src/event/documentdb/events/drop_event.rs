423f196834647e721ea5d14676b5c6fb975eb6cb
Morgan Nicholson <55922364+nichmorgan@users.noreply.github.com>
2023-10-12 13:11:52 -0300

    unused field removed

--- lambda-events/src/event/documentdb/events/drop_event.rs
+++ lambda-events/src/event/documentdb/events/drop_event.rs
@@ -1,7 +1,5 @@
-use super::commom_types::{AnyDocument, DatabaseCollection, DocumentId, DocumentKeyId, Timestamp};
+use super::commom_types::{AnyDocument, DatabaseCollection, DocumentId, Timestamp};
 use serde::{Deserialize, Serialize};
-
-// TODO: Campos pendentes, carece insumo de teste
 
 #[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -9,8 +7,6 @@ pub struct ChangeDropEvent {
     #[serde(rename = "_id")]
     id: DocumentId,
     cluster_time: Timestamp,
-    #[serde(default)]
-    document_key: Option<DocumentKeyId>,
     #[serde(default)]
     #[serde(rename = "lsid")]
     ls_id: Option<AnyDocument>,

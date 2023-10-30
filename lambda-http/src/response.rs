3e195f6b0c734ae694cb1375fed57cf1b6dd01fd
Harold Sun <sunhua@amazon.com>
2023-10-30 22:58:22 +0800

    Fixed media type suffix detection (#714)

--- lambda-http/src/response.rs
+++ lambda-http/src/response.rs
@@ -282,7 +282,9 @@ where
         }
 
         for suffix in TEXT_ENCODING_SUFFIXES {
-            if content_type.ends_with(suffix) {
+            let mut parts = content_type.trim().split(';');
+            let mime_type = parts.next().unwrap_or_default();
+            if mime_type.ends_with(suffix) {
                 return convert_to_text(self, content_type);
             }
         }
@@ -481,6 +483,24 @@ mod tests {
         assert_eq!(
             json,
             r#"{"statusCode":200,"headers":{"content-type":"application/json; charset=utf-16"},"multiValueHeaders":{"content-type":["application/json; charset=utf-16"]},"body":"〰〰〰","isBase64Encoded":false,"cookies":[]}"#
+        )
+    }
+
+    #[tokio::test]
+    async fn charset_content_type_header_suffix() {
+        // Drive the implementation by using `hyper::Body` instead of
+        // of `aws_lambda_events::encodings::Body`
+        let response = Response::builder()
+            .header(CONTENT_TYPE, "application/graphql-response+json; charset=utf-16")
+            .body(HyperBody::from("000000".as_bytes()))
+            .expect("unable to build http::Response");
+        let response = response.into_response().await;
+        let response = LambdaResponse::from_response(&RequestOrigin::ApiGatewayV2, response);
+
+        let json = serde_json::to_string(&response).expect("failed to serialize to json");
+        assert_eq!(
+            json,
+            r#"{"statusCode":200,"headers":{"content-type":"application/graphql-response+json; charset=utf-16"},"multiValueHeaders":{"content-type":["application/graphql-response+json; charset=utf-16"]},"body":"〰〰〰","isBase64Encoded":false,"cookies":[]}"#
         )
     }
 

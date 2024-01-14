b019840bad96d302369d587729f45f296a016096
kdnakt <a.kid.1985@gmail.com>
2024-01-14 16:30:55 +0900

    Add test for suppresor alarm

--- lambda-events/src/event/cloudwatch_alarms/mod.rs
+++ lambda-events/src/event/cloudwatch_alarms/mod.rs
@@ -301,4 +301,19 @@ mod test {
         let reparsed: CloudWatchCompositeAlarm = serde_json::from_slice(output.as_bytes()).unwrap();
         assert_eq!(parsed, reparsed);
     }
+
+    #[test]
+    #[cfg(feature = "cloudwatch_alarms")]
+    fn example_cloudwatch_alarm_composite_with_suppressor_alarm() {
+        let data = include_bytes!("../../fixtures/example-cloudwatch-alarm-composite-with-suppressor-alarm.json");
+        let parsed: CloudWatchCompositeAlarm = serde_json::from_slice(data).unwrap();
+        let state = parsed.alarm_data.state.clone().unwrap();
+        assert_eq!("WaitPeriod", state.actions_suppressed_by.unwrap());
+        assert_eq!("Actions suppressed by WaitPeriod", state.actions_suppressed_reason.unwrap());
+
+        let output: String = serde_json::to_string(&parsed).unwrap();
+        let reparsed: CloudWatchCompositeAlarm = serde_json::from_slice(output.as_bytes()).unwrap();
+        assert_eq!(parsed, reparsed);
+    }
+
 }

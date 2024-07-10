309e3abb4f266b502da67ac8e35ea8a6b531090d
Martin Jesper Low Madsen <martin@martinjlowm.dk>
2024-07-11 00:57:24 +0200

    Expand on the CloudTrail event (#909)
    * Expand on the CloudTrail event
    
    The initial contribution of the CloudTrail event was not complete, but rather
    constructed from a handful of event examples. The structure is documented at
    https://docs.aws.amazon.com/awscloudtrail/latest/userguide/cloudtrail-event-reference-user-identity.html
    which covers a number of optional fields and fields that are not already
    covered.
    
    * Add three example tests for CloudTrail events

--- lambda-events/src/event/cloudwatch_events/cloudtrail.rs
+++ lambda-events/src/event/cloudwatch_events/cloudtrail.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
@@ -26,23 +27,89 @@ pub struct AWSAPICall<I = Value, O = Value> {
 
 #[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
-pub struct UserIdentity {
+pub struct SessionIssuer {
     pub r#type: String,
+    pub user_name: Option<String>,
     pub principal_id: String,
     pub arn: String,
     pub account_id: String,
-    pub session_context: Option<SessionContext>,
 }
 
 #[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
-pub struct SessionContext {
-    pub attributes: Attributes,
+pub struct WebIdFederationData {
+    pub federated_provider: Option<String>,
+    pub attributes: Option<String>,
 }
 
 #[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Attributes {
     pub mfa_authenticated: String,
-    pub creation_date: String,
+    pub creation_date: DateTime<Utc>,
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SessionContext {
+    pub session_issuer: Option<SessionIssuer>,
+    pub web_id_federation_data: Option<WebIdFederationData>,
+    pub attributes: Attributes,
+    pub source_identity: Option<String>,
+    pub ec2_role_delivery: Option<String>,
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct OnBehalfOf {
+    pub user_id: String,
+    pub identity_store_arn: String,
+}
+
+// https://docs.aws.amazon.com/awscloudtrail/latest/userguide/cloudtrail-event-reference-user-identity.html
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct UserIdentity {
+    pub r#type: String,
+    pub account_id: Option<String>,
+    pub arn: Option<String>,
+    pub credential_id: Option<String>,
+    pub invoked_by: Option<String>,
+    pub principal_id: Option<String>,
+    pub session_context: Option<SessionContext>,
+    pub user_name: Option<String>,
+    pub on_behalf_of: Option<OnBehalfOf>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::AWSAPICall;
+
+    #[test]
+    #[cfg(feature = "cloudwatch_events")]
+    fn example_cloudwatch_cloudtrail_unknown_assumed_role() {
+        let data = include_bytes!("../../fixtures/example-cloudwatch-cloudtrail-assumed-role.json");
+        let parsed: AWSAPICall = serde_json::from_slice(data).unwrap();
+        let output: String = serde_json::to_string(&parsed).unwrap();
+        let reparsed: AWSAPICall = serde_json::from_slice(&output.as_bytes()).unwrap();
+        assert_eq!(parsed, reparsed);
+    }
+    #[test]
+    #[cfg(feature = "cloudwatch_events")]
+    fn example_cloudwatch_cloudtrail_unknown_federate() {
+        let data = include_bytes!("../../fixtures/example-cloudwatch-cloudtrail-unknown-federate.json");
+        let parsed: AWSAPICall = serde_json::from_slice(data).unwrap();
+        let output: String = serde_json::to_string(&parsed).unwrap();
+        let reparsed: AWSAPICall = serde_json::from_slice(&output.as_bytes()).unwrap();
+        assert_eq!(parsed, reparsed);
+    }
+    #[test]
+    #[cfg(feature = "cloudwatch_events")]
+    fn example_cloudwatch_cloudtrail_assumed_role() {
+        let data = include_bytes!("../../fixtures/example-cloudwatch-cloudtrail-unknown-user-auth.json");
+        let parsed: AWSAPICall = serde_json::from_slice(data).unwrap();
+        let output: String = serde_json::to_string(&parsed).unwrap();
+        let reparsed: AWSAPICall = serde_json::from_slice(&output.as_bytes()).unwrap();
+        assert_eq!(parsed, reparsed);
+    }
 }

024876ead980c2e5431f7169a32abc0aa4d873ff
Nick Angelou <angelou.nick@gmail.com>
2025-08-10 11:20:09 +0200

    update

--- lambda-http/src/lib.rs
+++ lambda-http/src/lib.rs
@@ -102,7 +102,7 @@ use std::{
 };
 
 mod streaming;
-pub use streaming::{into_streaming_response_inner, run_with_streaming_response};
+pub use streaming::{into_streaming_response, run_with_streaming_response};
 
 /// Type alias for `http::Request`s with a fixed [`Body`](enum.Body.html) type
 pub type Request = http::Request<Body>;
